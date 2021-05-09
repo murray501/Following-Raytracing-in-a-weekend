a85bd2e64b277a85708ce1f6a4d1ac3cf5b37570
murray501 <m68286832@gmail.com>
2021-05-09 18:19:56 +0900

    chapter11-2

--- chapter11/src/hitable.rs
+++ chapter11/src/hitable.rs
@@ -1,8 +1,10 @@
 use crate::myvec::Vec3;
 use crate::ray::Ray;
-use crate::material::Material;
+use crate::material::{Material, Lambertian, Metal, Dielectric};
 use std::rc::Rc;
 use rand::Rng;
+use rand::rngs::ThreadRng;
+
 pub struct HitRecord {
     pub t: f32,
     pub p: Vec3,
@@ -58,6 +60,16 @@ pub struct HitableList {
     pub list: Vec<Box<dyn Hitable>>,
 }
 
+impl HitableList {
+    fn add(&mut self, hitable: Box<dyn Hitable>) {
+        self.list.push(hitable);
+    }
+
+    fn length(&self) -> usize {
+        self.list.len()
+    }
+}
+
 impl Hitable for HitableList {
     fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
         let mut closest_so_far = t_max;
@@ -101,4 +113,56 @@ pub fn color(r: &Ray, world: &HitableList, depth: usize) -> Vec3 {
             Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t                 
         }
     }
+}
+
+pub fn random_scene() -> HitableList {
+    let mut list = HitableList::default();
+    let sphere = Sphere::new(Vec3::new(0., -1000., 0.), 1000.,
+            Rc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5))));
+    list.add(Box::new(sphere));
+    
+    let mut rng = rand::thread_rng();
+    
+    for a in -11..11 {
+        for b in -11..11 {
+            let center = Vec3::new(a as f32 + 0.9 * drand(&mut rng), 0.2, b as f32 + 0.9 * drand(&mut rng));
+            if (center - Vec3::new(4., 0.2, 0.)).length() > 0.9 {
+                let random = drand(&mut rng);
+                let hitable = 
+                    if random < 0.8 {
+                        let r = drand(&mut rng) * drand(&mut rng);
+                        let g = drand(&mut rng) * drand(&mut rng);
+                        let b = drand(&mut rng) * drand(&mut rng);
+                        Sphere::new(center, 0.2,
+                        Rc::new(Lambertian::new(Vec3::new(r, g, b))))
+                    } else if random < 0.95 {
+                        let x = 0.5 * (1.0 + drand(&mut rng));
+                        let y = 0.5 * (1.0 + drand(&mut rng));
+                        let z = 0.5 * (1.0 + drand(&mut rng));
+                        let fuzz = 0.5 * drand(&mut rng);
+                        Sphere::new(center, 0.2,
+                            Rc::new(Metal::new(Vec3::new(x, y, z), fuzz)))
+                    } else {
+                        Sphere::new(center, 0.2, 
+                            Rc::new(Dielectric::new(1.5)))
+                    };
+                list.add(Box::new(hitable));
+            }
+        }
+    }
+    
+    list.add(Box::new(Sphere::new(Vec3::new(0., 1., 0.), 1.0, 
+                Rc::new(Dielectric::new(1.5)))));
+
+    list.add(Box::new(Sphere::new(Vec3::new(-4., -1., 0.), 1.0,
+                    Rc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1))))));
+
+    list.add(Box::new(Sphere::new(Vec3::new(4., 1., 0.), 1.0,  
+                    Rc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0)))));            
+    
+    return list;
+}
+
+fn drand(rng: &mut ThreadRng) -> f32 {
+    rng.gen_range(0.0..1.0) as f32
 }
